2be7e76287e28dc5920b6a1eb4193b2abd73f57a
NotM3ry <nfaure.2007@outlook.fr>
2024-10-08 14:49:30 +0200

    jump

--- resources/player.py
+++ resources/player.py
@@ -19,7 +19,7 @@ class Player:
         self.world = world
         self.velocity_y = 0   # Vitesse verticale du joueur (initialement 0)
         self.jumping = False  # Indicateur de saut
-        self.jump_velocity = -4  # Vitesse initiale du saut (négative pour monter)
+        self.jump_velocity = -2  # Vitesse initiale du saut (négative pour monter)
         self.gravity = 0.1    # Accélération de la gravité (augmente la vitesse verticale)
         self.max_fall_speed = 4  # Limite la vitesse de chute (vitesse terminale)
 
